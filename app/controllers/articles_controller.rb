db78e70f8696bbe90aa15482570433631135745e
ReoFujishima <jplowling@gmail.com>
2018-08-22 11:02:24 +0900

    Add flash.now to article flash message

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,7 +12,7 @@ class ArticlesController < ApplicationController
     	flash[:success] = "Article has been created"
     	redirect_to articles_path
     else
-      flash[:danger] = "Article has not been created"
+      flash.now[:danger] = "Article has not been created"
       render :new
     end
   end
@@ -21,7 +21,6 @@ class ArticlesController < ApplicationController
 
 	  def article_params
 	  	params.require(:article).permit(:title, :body)
-	  	
 	  end
 
 end
